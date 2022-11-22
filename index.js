4ef979736607014fba2919f9586e209e0532d4ed
jackline-cheptanui <cheptanuijacklinee@gmail.com>
2022-11-22 11:06:15 +0300

    first commit

--- index.js
+++ index.js
@@ -37,9 +37,6 @@ function openW(){
 }
 
 
-
-
-
 function setNewImage(){
 document.getElementById("coff1").src="tonic.jpg";
 }
@@ -47,6 +44,8 @@ function setOldImage(){
    document.getElementById("coff1").src="coffee images/Cappucino.jpg"
    
 }
+
+
 
 
 function setNewImage1(){
@@ -65,16 +64,14 @@ function setNewImage1(){
            document.getElementById("coff3").src="coffee images/icecreamcoffee.jpg"
            
         }
-
-
-
+        
         function setNewImage3(){
             document.getElementById("coff4"). src="correto.jpg";
             }
             function setOldImage3(){
                document.getElementById("coff4").src="coffee images/Latte coffee.jpg"
                
-            }  
+            }
 
             function setNewImage4(){
                 document.getElementById("coff5"). src="affogato.jpg";
@@ -84,6 +81,7 @@ function setNewImage1(){
                    
                 }
 
+                
                 function setNewImage5(){
                     document.getElementById("coff6"). src="mocha.jpg";
                     }
